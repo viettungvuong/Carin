7f5b64422c1ba371b95ea63498c4b348a7ac6a71
viettungvuong <viettungvuong@gmail.com>
2024-09-01 10:31:41 +0700

    Change angle of bullet

---
+++ Assets/Scripts/Bullet/BulletControl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletControl : MonoBehaviour
+{
+    public int damage;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.name=="Terrain"){
+            gameObject.SetActive(false);
+            return;
+        }
+        GObject gObject = other.gameObject.GetComponent<GObject>();
+        if (gObject!=null){
+            gObject.TakeDamage(damage);
+        }
+    }
+}
