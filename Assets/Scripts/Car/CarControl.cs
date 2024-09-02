abfe6944739e28696eab88ec525d3c5e9acdd60f
viettungvuong <viettungvuong@gmail.com>
2024-09-03 02:08:14 +0700

    Create car

---
+++ Assets/Scripts/Car/CarControl.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class CarControl : MonoBehaviour
+{
+    [SerializeField] WheelCollider frontRight;
+    [SerializeField] WheelCollider frontLeft;
+    [SerializeField] WheelCollider backRight;
+    [SerializeField] WheelCollider backLeft;
+
+    [SerializeField] Transform frontRightTransform;
+    [SerializeField] Transform frontLeftTransform;
+    [SerializeField] Transform backRightTransform;
+    [SerializeField] Transform backLeftTransform;
+
+    public float acceleration = 500f;
+    public float brakingForce = 300f;
+    public float maxTurnAngle = 15f;
+
+    private float currentAcceleration = 0f;
+    private float currentBrakingForce = 0f;
+    private float currentTurnAngle = 0f;
+
+    private void FixedUpdate()
+    {
+        currentAcceleration = acceleration * Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.Space))
+            currentBrakingForce = brakingForce;
+        else
+            currentBrakingForce = 0f;
+
+        frontRight.motorTorque = currentAcceleration;
+        frontLeft.motorTorque = currentAcceleration;
+
+        frontRight.brakeTorque = currentBrakingForce;
+        frontLeft.brakeTorque = currentBrakingForce;
+        backLeft.brakeTorque = currentBrakingForce;
+        backRight.brakeTorque = currentBrakingForce;
+
+        currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
+        frontLeft.steerAngle = currentTurnAngle;
+        frontRight.steerAngle = currentTurnAngle;
+
+        UpdateWheel(frontLeft, frontLeftTransform);
+        UpdateWheel(frontRight, frontRightTransform);
+        UpdateWheel(backLeft, backLeftTransform);
+        UpdateWheel(backRight, backRightTransform);
+    }
+
+    void UpdateWheel(WheelCollider col, Transform trans)
+    {
+        Vector3 position;
+        Quaternion rotation;
+        col.GetWorldPose(out position, out rotation);
+
+        trans.position = position;
+        trans.rotation = rotation;
+    }
+}
