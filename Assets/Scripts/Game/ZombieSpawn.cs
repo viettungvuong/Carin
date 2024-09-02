f703dc05aa9118e29e6563b4e4f554bde2c3435b
viettungvuong <viettungvuong@gmail.com>
2024-09-02 15:16:28 +0700

    Change zombie physics

--- Assets/Scripts/Game/ZombieSpawn.cs
+++ Assets/Scripts/Game/ZombieSpawn.cs
@@ -8,13 +8,14 @@ public class ZombieSpawn : ObjectSpawn
 
     void Awake() {
         objectTag = "Zombie";
-        spawnNumber = 100;
+        spawnNumber = 10;
     }
 
     void Update()
     {
         // distance player has moved
         float distanceMoved = Vector3.Distance(lastPosition, transform.position);
+        Debug.Log(distanceMoved);
 
         // player has move more than spawn distance
         if (distanceMoved >= spawnDistance)
