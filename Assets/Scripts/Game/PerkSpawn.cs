14d4911c1ea3d74dc3d2a681c05e5c83755acddb
viettungvuong <viettungvuong@gmail.com>
2024-09-05 00:35:06 +0700

    Add refill collectibles on road

---
+++ Assets/Scripts/Game/PerkSpawn.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerkSpawn : MonoBehaviour
+{
+    // spawn perks (health, energy, bullets) on random positions
+    private string healthTag = "HealthRefill", energyTag = "EnergyRefill", bulletTag = "BulletRefill";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
