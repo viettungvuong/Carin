654fa28f4ca1f451d817e6484b5e05b78b36db5f
viettungvuong <viettungvuong@gmail.com>
2024-09-05 08:21:25 +0700

    Add collider

--- Assets/Scripts/Game/PerkSpawn.cs
+++ Assets/Scripts/Game/PerkSpawn.cs
@@ -4,17 +4,57 @@ using UnityEngine;
 
 public class PerkSpawn : MonoBehaviour
 {
-    // spawn perks (health, energy, bullets) on random positions
-    private string healthTag = "HealthRefill", energyTag = "EnergyRefill", bulletTag = "BulletRefill";
+    // Prefabs for the perks
+    public GameObject energyRefill;
+    public GameObject healthRefill;
+    public GameObject bulletRefill;
+
+
+    public Vector2 spawnAreaMin;
+    public Vector2 spawnAreaMax;
+
+
+    public int numberOfPerksToSpawn = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnPerks();
     }
 
-    // Update is called once per frame
-    void Update()
+    // spawn
+    void SpawnPerks()
     {
-        
+        for (int i = 0; i < numberOfPerksToSpawn; i++)
+        {
+            Vector2 randomPosition = GetRandomPosition();
+            GameObject perkToSpawn = GetRandomPerk();
+            Instantiate(perkToSpawn, randomPosition, Quaternion.identity);
+        }
+    }
+
+    // get random pos
+    Vector2 GetRandomPosition()
+    {
+        float randomX = Random.Range(spawnAreaMin.x, spawnAreaMax.x);
+        float randomY = Random.Range(spawnAreaMin.y, spawnAreaMax.y);
+        return new Vector2(randomX, randomY);
+    }
+
+    GameObject GetRandomPerk()
+    {
+        int randomIndex = Random.Range(0, 3); // 0 = energy, 1 = health, 2 = bullets
+
+        switch (randomIndex)
+        {
+            case 0:
+                return energyRefill;
+            case 1:
+                return healthRefill;
+            case 2:
+                return bulletRefill;
+            default:
+                return null;
+        }
     }
 }
